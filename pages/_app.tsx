7fc30a8057babac1f3e27151847ca3d3c13ad49d
Lawlietze <haoyu.wong@foxmail.com>
2022-07-08 17:06:58 +0800

    add type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
+import type { AppProps } from 'next/app'
 
-export default function MyApp({ Component, pageProps }): any {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
